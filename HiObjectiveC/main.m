10a3298076db076f14caa785283eec3c6ccbd1ef
Praj <prajna.kandarpa@uwaterloo.ca>
2015-03-06 17:52:11 -0500

    pointerss

--- HiObjectiveC/main.m
+++ HiObjectiveC/main.m
@@ -21,11 +21,28 @@
 // can be used to redefine existing types or add new data types.
 // after definition, they can be used like any other data type.
 typedef unsigned char ColorComponent;
+
+// STRUCTS
 typedef struct {
     unsigned char red;
     unsigned char green;
     unsigned char blue;
 } Color;
+
+// ENUMS
+// a collection of related constants
+// typedef enumerated types with a
+// descriptive name for convenience like structs
+
+typedef enum {
+    FORD,
+    HONDA,
+    CHEVROLET,
+    HYUNDAI,
+    MARUTI,
+    SUZUKI,
+    TOYOTA
+} CarModel;
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
@@ -106,9 +123,60 @@ int main(int argc, const char * argv[]) {
         ColorComponent green = 0;
         NSLog(@"Your paint job is R:%hhu, G:%hhu, B:%hhu", red, blue, green);
         
+        //To populate the new carColor structure, we used the {255, 160, 0}
+        //initializer syntax. This assigns values in the same order as they
+        // were declared in the struct.
         Color carColor = {255,160,0};
         NSLog(@"Your paint job is (R:%d, G:%d, B:%d)", carColor.red, carColor.green, carColor.blue);
-
+        
+        // myCar was decalred with the enumerated type CarModel, thus it can only hold one of the types
+        // that are a part of CarModel.
+        CarModel myCar = FORD;
+        switch (myCar) {
+            case FORD:
+                NSLog(@"You like Western cars you fascist pig");
+                //break;
+            case HONDA:
+                NSLog(@"Japan's really nice huh");
+                //break;
+                
+            default:
+                break;
+        }
+        
+        // PRIMITIVE ARRAYS
+        // obj c has access to primitive C arrays but its nicer to use the
+        // NSArray and NSMutableArray classes provided by the Foundation framework
+        // fall back to primitive arrays for performance intensive environments
+        int years[4] = {1980, 2010, 2012, 1299};
+        years[0] = 0000;
+        for (int i =0; i<4; i++) {
+            NSLog(@"The year right now is %d", years[i]);
+        }
+        
+        // POINTERS
+        // A direct reference to a memory address. variable is a transparent
+        // container for a value while pointers lets you see how the value is
+        // stored.
+        // & - reference operator that returns the memory address of a variable
+        // * - dereference operator returns the value stored in a memory address.
+        
+        int year = 1967;
+        int *pointer = &year;
+        *pointer = 1966;
+        NSLog(@"the value of pointer is %d. The value of point is %d", *pointer, year);
+        
+        // The real utility of pointers lies in the fact that a pointer can be moved to
+        // surrounding memory addresses. very useful for navigating arrays which are just
+        // contiguous blocks of memory.
+        
+        char model[5] = {'h', 'o', 'n', 'd', 'a'};
+        char *modPtr = &model[0];
+        for (int i=0; i<5; i++) {
+            NSLog(@"The value at memory address %p is %c", modPtr, *modPtr);
+            modPtr++;
+        }
+        NSLog(@"The first letter is %c", *(modPtr-5));
         
     }
     return 0;
