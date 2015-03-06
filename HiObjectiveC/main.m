5774de3e7bce7e81eeac6913634c9d55c331d6f9
Praj <prajna.kandarpa@uwaterloo.ca>
2015-03-06 15:24:41 -0500

    Initial Commit

---
+++ HiObjectiveC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HiObjectiveC
+//
+//  Created by Prajna Kandarpa on 2015-03-06.
+//  Copyright (c) 2015 Prajna Kandarpa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
